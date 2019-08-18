52a370a89854ce70770452069daed25bf793e795
Andrew Earls <andrewearls@rocketmail.com>
2019-08-18 14:34:07 -0700

    replaced tim

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -8,16 +8,16 @@
     <div class="col-xl-6">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
             <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                <a href="https://www.andrewearls.com" class="nav-link" target="_blank">Andrew Earls</a>
             </li>
             <li class="nav-item">
-                <a href="https://www.updivision.com" class="nav-link" target="_blank">Updivision</a>
+                <a href="#" class="nav-link" target="_blank"></a>
             </li>
             <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+                <a href="http://www.andrewearls.com/#about" class="nav-link" target="_blank">About Us</a>
             </li>
             <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+                <a href="#" class="nav-link" target="_blank"></a>
             </li>
             <li class="nav-item">
                 <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
